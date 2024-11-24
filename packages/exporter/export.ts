14e06fc3b032c9b8aa64de7913ac801e40b136ed
Siddhant Maitra <smaitra.dev@gmail.com>
2024-11-25 00:04:44 +0530

    wip: exporter refactor

--- packages/exporter/export.ts
+++ packages/exporter/export.ts
@@ -1,48 +1,108 @@
 import { constOpts, hitURL, SWIGGY_ORDER_URL } from "./utils";
-import {writeFileSync,mkdirSync,existsSync } from 'fs';
+import type { OrderItem, ExtractedOrder } from "./types";
 
-let offSetID : string = "";
-let timeoutID: Timer;
-const folderName : string = "exportdata";
+export async function exportNewData(lastOrderID: string | null, cookies: string, userAgent: string, offSetID: string = "") {
 
-export async function exportData(cookies : string){
+	const orders: ExtractedOrder[] = await exportData(cookies, userAgent, offSetID);
+
+	if (lastOrderID) {
+		const targetIndex = orders.findIndex((item: ExtractedOrder) => item.order_id === lastOrderID);
+
+		if (targetIndex) {
+			const precedingItems = orders.slice(0, targetIndex);
+			return precedingItems;
+		}
+		return orders;
+
+	}
+	return orders;
+}
+
+
+async function exportData(cookies: string, userAgent: string, offSetID: string = "") {
 	let options = structuredClone(constOpts);
-	
+
 	const headers = new Headers(options.headers);
-	headers.set("Cookie",cookies);
-	headers.set("Content-Type","application/json");
+	headers.set("Cookie", cookies);
+	headers.set("Content-Type", "application/json");
+	headers.set("User-Agent", userAgent);
 
 	options.headers = headers;
-	
-	let response = await hitURL(`${SWIGGY_ORDER_URL}${offSetID}`, options);
 
+	let response = await hitURL(`${SWIGGY_ORDER_URL}${offSetID}`, options);
 	let res = await response.json();
-	console.log("Response from Swiggy: ", res?.data);
-	
+
 	let orderArray = await res.data?.orders;
-	
+
 	console.log("orderArray creation done. Length is ", orderArray.length);
+
+	if (res.statusCode != 0) {
+		throw new Error(`Order fetch failed. ${res.statusCode}`);
+	}
 
 	if (res && (res.statusCode === 0) && orderArray?.length > 0) {
 		offSetID = orderArray[orderArray.length - 1]?.order_id; //use .at(-1)?
-		
-		console.log(`New offset ID: ${offSetID}\n`);
 
-		try {
-			if (!existsSync(folderName)) {
-			  mkdirSync(folderName);
-			}
-		  } catch (err) {
-			console.error(err);
-		  }
-		writeFileSync(`${folderName}/orders_${offSetID}.json`, JSON.stringify(orderArray, null, 2));
+		const processedOrders = orderArray.map(extractOrderInfo);
 
-		timeoutID = setTimeout(() =>{
-			exportData(cookies);
-		}, 5000);
+		return processedOrders;
 
 	} else {
-		clearTimeout(timeoutID);
 		console.log(`Order fetch stopped`);
 	}
+}
+
+function extractOrderInfo(order: any) {
+	const renderingDetails = order.rendering_details.reduce((acc: any, detail: any) => {
+		if (detail.type === 'display' || detail.type === 'summary') {
+			acc[detail.key] = detail.value;
+		}
+		return acc;
+	}, {});
+
+	const extractedOrder = {
+		order_id: order.order_id,
+		order_date: new Date(order.ordered_time_in_seconds * 1000).toISOString(),
+		delivery_date: new Date(order.delivered_time_in_seconds * 1000).toISOString(),
+		restaurant_id: order.restaurant_id,
+		restaurant_name: order.restaurant_name,
+		restaurant_city: order.restaurant_city_name,
+		restaurant_cuisine: order.restaurant_cuisine,
+		items: order.order_items.flatMap((item: OrderItem) => {
+			if (item.has_variantv2) {
+				return item.variants.map(variant => ({
+					name: `${item.name} - ${variant.name}`,
+					price: variant.price.toString()
+				}));
+			} else {
+				return [{
+					name: item.name,
+					base_price: item.base_price.toString(),
+					quantity: item.quantity
+				}];
+			}
+		}),
+		coupon_applied: order.coupon_applied,
+		order_status: order.order_status,
+		is_long_distance: order.is_long_distance,
+		on_time: order.on_time,
+		rain_mode: order.rain_mode,
+		is_veg: order.order_items.every((item: OrderItem) => item.is_veg === "1"),
+		is_gourmet: order.is_gourmet,
+		rating_meta: {
+			restaurant_rating: order.rating_meta.restaurant_rating.rating,
+			delivery_rating: order.rating_meta.delivery_rating.rating
+		},
+		item_total: renderingDetails.item_total,
+		order_packing_charges: renderingDetails.order_packing_charges,
+		platform_fees: renderingDetails.partial_platform_fees || renderingDetails.platform_fees,
+		delivery_charges: renderingDetails.delivery_charges_swiggy_one,
+		discount_applied: renderingDetails.discount_applied_coupon,
+		total_taxes: renderingDetails.total_taxes,
+		order_total_string: renderingDetails.order_total_string,
+		paymentMethod: order.paymentTransactions[0].paymentMethod,
+		paymentGateway: order.paymentTransactions[0].paymentGateway
+	};
+
+	return extractedOrder;
 }
