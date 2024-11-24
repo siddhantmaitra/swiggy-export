0f7b4bb79e939d2917a04fca24b635652df8fde1
Siddhant Maitra <smaitra.dev@gmail.com>
2024-11-24 21:03:34 +0530

    refactor : login functions
    Login functions now takes arguments for data such as user-agent,
    cookies, and other tokens. This allows the login flow to be stateless

--- packages/exporter/index.ts
+++ packages/exporter/index.ts
@@ -1,10 +1,1 @@
-import { exportData } from "./export";
-import { login } from "./login";
-
-
-async function main(){
-	const cookies = await login();
-	await exportData(cookies);
-}
-
-main();
+export { visitSwiggy, generateOTP, performLogin } from "./login";
