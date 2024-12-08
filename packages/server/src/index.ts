6183352a0c6c57db1b41b3fdaf8aa2a5f19fa895
Siddhant Maitra <smaitra.dev@gmail.com>
2024-12-09 02:05:19 +0530

    feat: server global error handling

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,39 +1,31 @@
 import { Hono } from 'hono';
 import * as exporter from 'exporter';
-import { HTTPException } from 'hono/http-exception';
+import SwiggyError from 'exporter/SwiggyError';
 
 const app = new Hono();
 
-// app.get('/', (c) => {
-//   const ua = c.req.header('User-Agent') ?? "";
-//   let data = {};
-//   console.log(ua);
-//   exporter.visitSwiggy(ua).then(res => data = res);
-
-//   return c.text(JSON.stringify(data));
-// })
-app.get('/', async (c) => {
-	const ua = c.req.header('User-Agent') ?? '';
-	let data: any = {};
-
-	try {
-		data = await exporter.visitSwiggy('');
-		c.res.headers.set('Set-Cookie', `request_cookies=${data.requestCookies};`);
-		c.res.headers.set('Set-Cookie', `request_csrf=${data.csrf};`);
-	} catch (err: any) {
-		data = err.cause;
-		throw new HTTPException(err.cause.code, err.cause);
+app.onError((err, c) => {
+	if (err instanceof SwiggyError) {
+		return c.json(
+			{
+				status: 'Failure',
+				error: err.message,
+				code: err.statusCode,
+				cause: err.reason || null,
+			},
+			err.statusCode === 400 ? 400 : 500
+		);
 	}
 
-	return c.html(
-		`<h1>Hello from swiggy-export!</h1><div><p>Received data for login: </p> <div>${JSON.stringify(data)}</div></div>`
+	// Generic error handler for other error types
+	return c.json(
+		{
+			error: 'Internal Server Error',
+			message: err.message,
+			cause: err.cause,
+		},
+		500
 	);
 });
 
-app.onError((err, c) => {
-	if (err instanceof HTTPException) {
-		console.error(`${err}`);
-	}
-	return c.text('Custom Error Message', 500);
-});
 export default app;
