45a395cc26afd7600bd1521435e78d6f4679788a
Siddhant Maitra <smaitra.dev@gmail.com>
2024-12-14 01:34:51 +0530

    fastcommit

--- .github/scripts/server-build-release.sh
+++ .github/scripts/server-build-release.sh
@@ -10,10 +10,9 @@ cd packages/server
 bun install
 # bun build --compile src/index.ts --target=bun-linux-x64 --outfile=swm
 bun build src/index.ts --compile --minify --sourcemap --bytecode --target=bun-linux-x64 --outfile=swm
-cd ../.. 
-ls -alR 
-# Create GitHub release
-gh release create server-v$VERSION \
-  swm \
-  --title "Server v$VERSION" : CI test Release \
-  --notes "Testing CI release of the server package. Please Ignore"
+
+# # Create GitHub release
+# gh release create server-v$VERSION \
+#   swm \
+#   --title "Server v$VERSION" : CI test Release \
+#   --notes "Testing CI release of the server package. Please Ignore"
