1eb778dc856526611e6cc586f534657dd54b5cd1
Siddhant Maitra <smaitra.dev@gmail.com>
2024-12-22 02:59:14 +0530

    fastcommit: Sun Dec 22 02:59:14 IST 2024

--- .github/scripts/server-build-release.sh
+++ .github/scripts/server-build-release.sh
@@ -6,13 +6,7 @@ VERSION=$1
 
 # Build the server binary
 bun install
-# bun build --compile src/index.ts --target=bun-linux-x64 --outfile=swm
-bun build packages/server/src/index.ts --compile --minify --sourcemap --bytecode --target=bun-linux-x64 --outfile=swm-${VERSION}
 
-bun build packages/server/src/index.ts --compile --minify --sourcemap --bytecode --target=bun-windows-x64-modern --outfile=swm-win-${VERSION}
+bun NODE_ENV=$2 build packages/server/src/index.ts --compile --minify --sourcemap --bytecode --target=bun-linux-x64 --outfile=swm-${VERSION}
 
-# # Create GitHub release
-# gh release create server-v$VERSION \
-#   swm \
-#   --title "Server v$VERSION" : CI test Release \
-#   --notes "Testing CI release of the server package. Please Ignore"
+bun NODE_ENV=$2 build packages/server/src/index.ts --compile --minify --sourcemap --bytecode --target=bun-windows-x64-modern --outfile=swm-win-${VERSION}
