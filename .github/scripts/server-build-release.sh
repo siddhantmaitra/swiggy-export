3a3d448d548197af5a4dc9dbd2359c480409f28f
Siddhant Maitra <smaitra.dev@gmail.com>
2024-12-22 03:18:17 +0530

    fastcommit: Sun Dec 22 03:18:17 IST 2024

--- .github/scripts/server-build-release.sh
+++ .github/scripts/server-build-release.sh
@@ -7,6 +7,6 @@ VERSION=$1
 # Build the server binary
 bun install
 
-bun NODE_ENV=$2 build packages/server/src/index.ts --compile --minify --sourcemap --bytecode --target=bun-linux-x64 --outfile=swm-${VERSION}
+NODE_ENV=$2 bun build packages/server/src/index.ts --compile --minify --sourcemap --bytecode --target=bun-linux-x64 --outfile=swm-${VERSION}
 
-bun NODE_ENV=$2 build packages/server/src/index.ts --compile --minify --sourcemap --bytecode --target=bun-windows-x64-modern --outfile=swm-win-${VERSION}
+NODE_ENV=$2 bun build packages/server/src/index.ts --compile --minify --sourcemap --bytecode --target=bun-windows-x64-modern --outfile=swm-win-${VERSION}
