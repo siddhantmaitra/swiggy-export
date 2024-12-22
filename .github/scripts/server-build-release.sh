fa2f6e2eebd366e7e10022899eb316801018e884
Siddhant Maitra <smaitra.dev@gmail.com>
2024-12-23 01:00:48 +0530

    fastcommit: Mon Dec 23 01:00:48 IST 2024

--- .github/scripts/server-build-release.sh
+++ .github/scripts/server-build-release.sh
@@ -9,6 +9,13 @@ bun install
 
 echo "FOUND NODE_ENV: ${NODE_ENV}"
 
-NODE_ENV=$NODE_ENV bun build packages/server/src/index.ts --compile --minify --sourcemap --bytecode --target=bun-linux-x64 --outfile=swm-${VERSION}
+NODE_ENV=$NODE_ENV bun build packages/server/src/index.ts \
+  --define "process.env.NODE_ENV='${NODE_ENV}'" \
+  --compile \
+  --minify \
+  --sourcemap \
+  --bytecode \
+  --target=bun-linux-x64 \
+  --outfile="swm-${VERSION}"
 
 NODE_ENV=$NODE_ENV bun build packages/server/src/index.ts --compile --minify --sourcemap --bytecode --target=bun-windows-x64-modern --outfile=swm-win-${VERSION}
